085255fa6b90ca5ced195fdd0b17dd30bacdaec5
Sawako <inmortaldragonxspace@outlook.com>
2020-09-22 19:16:35 -0600

    [REFACTOR]: La API de Discord no deja borrar mas de 100 mensajes por lo tanto el comando esta limitado a 100.

--- src/commands/mod/bulk.message.ts
+++ src/commands/mod/bulk.message.ts
@@ -13,11 +13,11 @@ import { ErrorService } from '../../utils/services';
 export abstract class BanCommand {
   private _errorService: ErrorService = new ErrorService();
 
-  @Command('m bulk :type :total')
+  @Command('m bulk :total')
   @Infos({ forAdmins: true })
   @Description('Comando para banear usuarios.')
   async Ban(command: CommandMessage) {
-    const { total, type } = command.args;
+    const { total } = command.args;
     const embedMessage = new MessageEmbed();
     embedMessage.setColor(
       EmbedColorsArray[Math.floor(Math.random() * EmbedColorsArray.length)],
@@ -34,49 +34,36 @@ export abstract class BanCommand {
       );
     }
 
-    if (isNaN(total) && type == '-s') {
+    if (isNaN(total)) {
       return await command.channel.send(
         this._errorService.showError(
-          'Como elegiste el tipo de **-s** por favor necesito saber cuantos mensajes tengo que borrar en numeros.',
+          'Por favor necesito saber cuantos mensajes tengo que borrar en numeros.',
+        ),
+      );
+    }
+
+    if (total > 100) {
+      return await command.channel.send(
+        this._errorService.showError(
+          'No puedo borrar mas de 100 mensajes, perdoname por favor. (∩︵∩)',
         ),
       );
     }
 
     try {
-      switch (type) {
-        case '-a':
-          const messagesChannel = (
-            await command.channel.messages.fetch({ limit: 100 })
-          ).size;
+      embedMessage.setTitle('Mensajes selecionados eliminados');
+      embedMessage.setDescription(
+        `Muy bien, he eliminado un total de ${total} mensajes.`,
+      );
+      embedMessage.setFooter('Buena limpieza has hecho Onni-Chan');
 
-          embedMessage.setTitle('Mensajes eliminados');
-          embedMessage.setDescription(
-            'Muy bien, he eliminado un total de 100 mensajes.',
-          );
-          embedMessage.setFooter('Buena limpieza has hecho Onni-Chan');
-
-          await command.delete();
-          await command.channel.messages.channel.bulkDelete(messagesChannel);
-          await command.channel.send(embedMessage);
-          break;
-        case '-s':
-          embedMessage.setTitle('Mensajes selecionados eliminados');
-          embedMessage.setDescription(
-            `Muy bien, he eliminado un total de ${total} mensajes.`,
-          );
-          embedMessage.setFooter('Buena limpieza has hecho Onni-Chan');
-
-          await command.delete();
-          await command.channel.messages.channel.bulkDelete(total);
-          await command.channel.send(embedMessage);
-          break;
-        default:
-          break;
-      }
+      await command.delete();
+      await command.channel.messages.channel.bulkDelete(total);
+      await command.channel.send(embedMessage);
     } catch (error) {
       await command.channel.send(
         this._errorService.showError(
-          'Oh no, no he podido eliminar los mensajes que querias porque tuve un problema, por favor intentalo de nuevo. UnU',
+          'Oh no, no he podido eliminar los mensajes que querias porque tuve un problema, seguramente fue porque querias eliminar mas de 100, no puedo con mas de esos, son demasiados. UnU',
         ),
       );
     }
